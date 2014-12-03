c8fc984a5dbb211838a51827a80ce133cdac75e2
gilbertopjunior <Gilberto.ux@gmail.com>
2014-12-03 04:26:01 -0200

    Desafio 2
    Inserção de graficos concluido.
    
    Alteração nos padrões das pastas

--- js/page02.js
+++ js/page02.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 
-    $.getJSON('json/dados-edeb-santa-catarina.json', function(data) {
+    //define chart colours
+    var dynaColorsSet = [
+        "#ffd614", // yellow
+        "#fd8317", // orange
+        "#b8df26", // green
+        "#34a1de" // blue
+    ];
+
+    $.getJSON('js/data/dados-edeb-santa-catarina.json', function(data) {
 
         $.each(data.municipio, function(index, val) {
 
@@ -8,16 +16,41 @@ $(document).ready(function() {
 
             $("#table-ideb ").append(
 
-                "<tr>" +
+
+                "<tr class='new-line'>" +
                 "<td>" + "<div class='nome-escola'>" + val.nome + "</div>" + "<span>" + val.escolas + "</span>" + "</td>" +
                 "<td>" + "<div class='fxa'>" + val.fluxo + " * " + val.aprendizado + " = " + fxa + "</div>" + "</td>" +
-                "<td>" + val.alerta + "</td>" +
-                "<td>" + val.atencao + "</td>" +
-                "<td>" + val.melhorar + "</td>" +
-                "<td>" + val.manter + "</td>" +
+                "<td class='ln-char'>" + "<div class='chart yellow' data-percent='" + val.alerta + "'>" + "<span class='percent'>" + "</span>" + "</div>" + "</td>" +
+                "<td class='ln-char'>" + "<div class='chart orange' data-percent='" + val.atencao + "'>" + "<span class='percent'>" + "</span>" + "</div>" + "</td>" +
+                "<td class='ln-char'>" + "<div class='chart green' data-percent='" + val.melhorar + "'>" + "<span class='percent'>" + "</span>" + "</div>" + "</td>" +
+                "<td class='ln-char'>" + "<div class='chart blue' data-percent='" + val.manter + "'>" + "<span class='percent'>" + "</span>" + "</div>" + "</td>" +
                 "</tr>"
 
             )
+
+            $('.chart').easyPieChart({
+                barColor: function() {
+                    if ($(this.el).hasClass('yellow')) {
+                        return dynaColorsSet[0];
+                    } else if ($(this.el).hasClass('orange')) {
+                        return dynaColorsSet[1];
+                    } else if ($(this.el).hasClass('green')) {
+                        return dynaColorsSet[2];
+                    } else {
+                        return dynaColorsSet[3];
+                    }
+
+                },
+                trackColor: '#EFEFEF  ',
+                scaleColor: false,
+                lineWidth: 10,
+                size: 80,
+                lineCap: 'square',
+                onStep: function(from, to, percent) {
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+
 
         });
 
